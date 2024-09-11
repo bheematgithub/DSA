229a62bfd65f276096716be7b11974a6353ad4ff
bheematgithub <bhimashiplc@gmail.com>
2024-09-11 21:35:01 +0530

    11/09/2024

---
+++ 2220.Minimum_Bit_Flips_to_Convert_Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        xor = start ^ goal
+        ans = 0
+        while xor > 0:
+            ans += xor & 1
+            xor >>= 1
+        return ans
